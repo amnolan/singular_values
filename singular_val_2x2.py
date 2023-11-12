f52bb83530424e634c470a35f0701bf973bf9e7a
A.M. Nolan <11098245+amnolan@users.noreply.github.com>
2023-11-12 09:32:33 -0600

    Update singular_val_2x2.py works

--- singular_val_2x2.py
+++ singular_val_2x2.py
@@ -1,3 +1,4 @@
+import math
 def convert_to_float(input_row):
     output_row = []
     for i in input_row:
@@ -12,22 +13,6 @@ def two_d_deep_copy(src_array):
             sub_ra.append(src_array[i][j])
         dest_array.append(sub_ra)
     return dest_array
-
-def total_up_mean_matrix(ra):
-    totals_list = []
-    for i in range(0,len(ra)):
-        total = 0
-        for j in range(0,len(ra[i])):
-            total += ra[i][j]
-        totals_list.append(total)
-    return totals_list
-
-def get_x_hat_array(ra,mean_vector):
-    #iterate over vectors
-    for i in range(0,len(ra[0])):
-        ra[0][i] = (ra[0][i] - mean_vector[0])
-        ra[1][i] = (ra[1][i] - mean_vector[1])
-    return ra
 
 def transpose_matrix(matrix):
 
@@ -61,41 +46,21 @@ def matrix_multiply(matrix1, matrix2):
 
     return result_matrix
 
-def multiply_matrix_by_scalar(matrix, scalar):
-    # Iterate through the matrix and multiply each element by the scalar
-    result_matrix = [[element * scalar for element in row] for row in matrix]
-    return result_matrix
-
-def sample_mean(matrix, n):
-    top = []
-    bottom = []
-    container = []
-    for count in range(0,n):
-        top.append((1/n)*parent_ra[0][count])
-        bottom.append((1/n)*parent_ra[1][count])
-    container.append(top)
-    container.append(bottom)
-    return container
-
 def find_eigenvalues(matrix):
     try:
-        # Check if the matrix is 2x2
+        
         if len(matrix) != 2 or len(matrix[0]) != 2 or len(matrix[1]) != 2:
             raise ValueError("Input matrix must be a 2x2 matrix")
 
-        # Get matrix elements
         a, b, c, d = matrix[0][0], matrix[0][1], matrix[1][0], matrix[1][1]
 
-        # Calculate eigenvalues using the quadratic formula
         discriminant = (a + d)**2 - 4 * (a * d - b * c)
 
         if discriminant >= 0:
-            # Real eigenvalues
             eigenvalue1 = (a + d + discriminant**0.5) / 2
             eigenvalue2 = (a + d - discriminant**0.5) / 2
             return eigenvalue1, eigenvalue2
         else:
-            # Complex eigenvalues
             real_part = (a + d) / 2
             imaginary_part = abs(discriminant)**0.5 / 2
             eigenvalue1 = complex(real_part, imaginary_part)
@@ -103,7 +68,7 @@ def find_eigenvalues(matrix):
             return eigenvalue1, eigenvalue2
 
     except Exception as e:
-        return f"Error: {str(e)}"
+        return print("Error: ",str(e))
 
 print("Singular Values")
 print("Find A^T*A then find eigenvals")
@@ -122,6 +87,12 @@ parent_ra.append(bottom_row)
 original_ra = two_d_deep_copy(parent_ra)
 a_t = transpose_matrix(parent_ra)
 a_t_a = matrix_multiply(parent_ra,a_t)
+print("A^T*A\n")
 print(a_t_a)
-
-find_eigenvalues
+eigenvals = find_eigenvalues(a_t_a)
+print("Eigenvals:\n")
+print(eigenvals)
+print("eigenval sqrt() o-1 =")
+print(math.sqrt(eigenvals[0]))
+print("eigenval sqrt() o-2 =")
+print(math.sqrt(eigenvals[1]))
